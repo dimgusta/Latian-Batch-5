dc95fcfff13a0ee6ec20a83302b8cfe938abacf9
dimgusta <dimgusta@gmail.com>
2021-03-21 13:49:53 +0800

    Kedua

---
+++ 3. Pertemuan Ketiga/Example.py
@@ -0,0 +1,9 @@
+data = int(input("Jumlah Data = "))
+value = []
+
+for n in range(data) :
+    nama = input("Nama = ")
+    umur = int(input("Umur = "))
+    value.append(nama)
+    value.append(umur)
+print (value)
---
+++ 3. Pertemuan Ketiga/Perulangan_for.py
@@ -0,0 +1,14 @@
+for n in range(3):
+    print("apa ini?")
+
+for m in range(0, 3, 1):
+    print("apa ini?")
+    
+fruit = ["sirsak","apple", "cerry", "banana", "melon"]
+for x in fruit:
+    print(x)
+
+
+
+
+
