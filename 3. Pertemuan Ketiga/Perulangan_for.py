06d3c1c1046ff0ed63dfb91e5e79ba8948493502
dimgusta <dimgusta@gmail.com>
2021-03-21 13:57:54 +0800

    Latihan Lagi

--- 3. Pertemuan Ketiga/Perulangan_for.py
+++ 3. Pertemuan Ketiga/Perulangan_for.py
@@ -1,14 +1,13 @@
-for n in range(3):
+for n in range(3): #mengulang var m sebanyak 3 kali
     print("apa ini?")
 
-for m in range(0, 3, 1):
+for m in range(0, 3, 1): #mengulang var m sebanyak 0-3 kali dengan step 1
     print("apa ini?")
     
 fruit = ["sirsak","apple", "cerry", "banana", "melon"]
 for x in fruit:
     print(x)
 
-
-
-
-
+fruit1 = ["sirsak","apple", "cerry", "banana", "melon"]
+for a in fruit1[0:2]:
+    print(a)
